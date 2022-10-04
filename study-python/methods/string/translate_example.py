bfa5ef9eb856bbf90df332d224f221c0af9a1303
Daniel Choi <schoiak@connect.ust.hk>
2022-10-05 01:11:48 +0800

    Comment - comment added

--- study-python/methods/string/translate_example.py
+++ study-python/methods/string/translate_example.py
@@ -2,7 +2,11 @@
 
 mystr = 'Tutorials Teacher'
 trans_dict = {'T':'1','o':'2','l':'3','h':'4'}
+
+# create translator
 mytable = mystr.maketrans(trans_dict)
+
+# convert string using the transltor
 mystr_translated = mystr.translate(mytable)
 
 print('Original String:', mystr)
