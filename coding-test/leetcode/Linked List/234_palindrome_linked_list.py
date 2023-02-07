7339275046c9da5c39c3f9f5026bd7bafb062cf4
CHOI, Seung Ryeol <schoiak@connect.ust.hk>
2023-02-08 00:28:34 +0900

    Added - leetcode link

--- coding-test/leetcode/Linked List/234_palindrome_linked_list.py
+++ coding-test/leetcode/Linked List/234_palindrome_linked_list.py
@@ -1,8 +1,12 @@
+# https://leetcode.com/problems/palindrome-linked-list/
+
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         a = []
--- coding-test/leetcode/Linked List/328_odd_even_listked_list.py
+++ coding-test/leetcode/Linked List/328_odd_even_listked_list.py
@@ -1,9 +1,13 @@
-# Definition for singly-linked list.
+# https://leetcode.com/problems/odd-even-linked-list/
+
 from typing import Optional
+
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+        
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head: return head
