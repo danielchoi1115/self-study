b85f76675f5d80d73a3a0dc8a68b1bc2bc4e654a
Daniel Choi <schoiak@connect.ust.hk>
2023-03-19 17:35:12 +0900

    Add - baekjoon coding

--- coding-test/baekjoon/bronze3/14568.py
+++ coding-test/baekjoon/bronze3/14568.py
@@ -1,7 +1,4 @@
 # 2017 연세대학교 프로그래밍 경시대회 
 # https://www.acmicpc.net/problem/14568
 N = int(input())
-ans = 0
-for t in range(2,N-1, 2):
-    ans += (N-t-2)//2
-print(ans)
+print(sum((N-t-2)//2 for t in range(2,N-1, 2)))
--- coding-test/baekjoon/bronze3/6131.py
+++ coding-test/baekjoon/bronze3/6131.py
@@ -6,9 +6,4 @@ N = int(sys.stdin.readline().strip())
 powerList = [pow(i, 2) for i in range(1,501)]
 sub = {p-N for p in powerList if p > N}
 
-count = 0
-for p in powerList:
-    if p in sub:
-        count += 1
-
-print(count)
+print(sum(p in sub for p in powerList))
