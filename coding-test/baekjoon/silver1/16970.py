6ef8eaf097c5bfaaa03ab7769fa9588e41f26db3
Daniel Choi <schoiak@connect.ust.hk>
2023-03-30 14:55:13 +0900

    Add - Baekjoon 23-03-30

--- coding-test/baekjoon/silver1/16970.py
+++ coding-test/baekjoon/silver1/16970.py
@@ -9,21 +9,10 @@ N, M, K = map(int, input().split())
 t1 = time()
 def isValid(x, y):
     return x >= 0 and y >= 0 and x <= N and y <= M
-S = set()
+s=  0
 for x_from, y_from in itertools.product(range(N+1), range(M+1)):
     for x_to, y_to in itertools.product(range(N+1), range(M+1)):
-        if x_from == x_to and y_from == y_to: continue
-        dx = x_to - x_from
-        dy = y_to - y_from
-        
-        gcd = math.gcd(dx, dy)
-        slope_x = dx // gcd
-        slope_y = dy // gcd
-
-        x_k = x_from + slope_x * (K - 1)
-        y_k = y_from + slope_y * (K - 1)
-
-        if isValid(x_k, y_k):
-            S.add(frozenset({(x_from, y_from), (x_k, y_k)}))
+        if math.gcd(abs(x_from-x_to), abs(y_from-y_to)) == K - 1:
+            s += 1
 print(time()-t1)
-print(len(S))
+print(s//2)
