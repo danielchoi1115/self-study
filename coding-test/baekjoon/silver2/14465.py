bc94ad51ad7adaa13ea6101b2b09987ff223fd3f
Daniel Choi <schoiak@connect.ust.hk>
2023-04-06 21:56:55 +0900

    Mod - performance improvement

--- coding-test/baekjoon/silver2/14465.py
+++ coding-test/baekjoon/silver2/14465.py
@@ -2,15 +2,16 @@
 # [14465] 소가 길을 건너간 이유 5
 # https://www.acmicpc.net/problem/14465
 import sys
-N, K, ans = map(int, input().split())
+N, r, ans = map(int, input().split())
 broken = set(map(int, sys.stdin.read().split()))
-cnt = 0
-for i in range(1, N):
-    lst = i-K+1
-    nxt = i
-    if lst > 0 and lst in broken: 
+cnt = sum(s in broken for s in range(1, r+1))
+l = 1
+while r < N:
+    if l in broken: 
         cnt -= 1
-    if nxt in broken: 
+    l += 1
+    r += 1
+    if r in broken: 
         cnt += 1
-    ans = min(ans, cnt) if lst > 0 else ans
+    ans = min(ans, cnt)
 print(ans)
